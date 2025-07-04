a3ee847c5512282fa7fb96c6ced27412f3b2ec94
idowu moses <dynamopraize5050@gmail.com>
2025-07-04 12:33:53 +0100

    update project

--- MyDatabaseFile/MI-CH02 Database Project/questionL.sql
+++ MyDatabaseFile/MI-CH02 Database Project/questionL.sql
@@ -1,5 +1,4 @@
-select City, Store, count(*) as numberOfPurchase
+select count(City*Store) as numberOfPurchase
 from item
-group by City, Store
 
 -- Count the number of purchases having each combination of City and Store.
